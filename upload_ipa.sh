20d0b4901f98ed1ec83f870921347a6f5b6301d6
and <77030548+exception7601@users.noreply.github.com>
2025-09-22 09:28:39 -0300

    Refactor upload_ipa.sh to remove redundancy
    Removed duplicate call to ota.sh and wrangler deploy.

--- upload_ipa.sh
+++ upload_ipa.sh
@@ -7,15 +7,16 @@ fastlane ios adhoc
 
 BUILD=$(date +%s) 
 NEW_VERSION=19.${BUILD}
+
+./ota.sh $NEW_VERSION
+
 echo $NEW_VERSION > version
 
+git add  public/manifest.plist
 git add version
 git commit -m "new Version ${NEW_VERSION}"
 git tag -s -a ${NEW_VERSION} -m "v${NEW_VERSION}"
 
 git push origin HEAD --tags
 gh release create ${NEW_VERSION} ${NAME} --notes "v${NEW_VERSION}"
-
-./ota.sh $NEW_VERSION
-wrangler deploy
 
